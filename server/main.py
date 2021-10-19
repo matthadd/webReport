d68e3ff206961cfc73419a407af2b8f2273c1b93
matthadd <matthis.haddouche@gmail.com>
2021-10-19 18:36:55 +0200

    change fastapi to flask

--- server/main.py
+++ server/main.py
@@ -1,32 +1,14 @@
-from fastapi import FastAPI, Response
-import json
-from users import *
-from projects import *
-
-app = FastAPI()
+from flask import Flask
 
 
-def fill_countries_datamodel():
-    return 0
+app = Flask(__name__)
 
 
-@app.get('/')
-async def root():
-    # describe the endpoints for the user
-    return {'message': 'Welcome to countries geodata visualisation',
-            'endpoints': {'/iso_code': 'POST', '/all_geometries': 'GET'}}
+@app.route("/")
+
+def index():
+
+    return "Hello this is the new version!"
 
 
-@app.post('/iso_code')
-# takes as input one or multiple country names and returns the ISO3 country codes
-# and return the associated geographical data when the optional parameter ‘details’ is True
-# async def iso_code(iso_code_request: IsoCodeRequest):
-#
-# 
-#     return 0
-
-
-@app.get('/all_geometries')
-# retrieve all the contents of the countries.geojson file in one go
-async def all_geometries():
-    return fill_countries_datamodel()
+app.run()
--- server/start.sh
+++ server/start.sh
@@ -1,1 +1,1 @@
-uvicorn main:app --reload
+python3 main.py
