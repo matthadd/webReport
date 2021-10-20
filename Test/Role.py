9c153bfeeaaf4911864d57da22cf02ec3c9322ae
HU Mazhou <mazhouhu97@gmail.com>
2021-10-20 16:52:12 +0200

    modification User.dia

---
+++ Test/Role.py
@@ -0,0 +1,13 @@
+
+class Role:
+    """Class Role
+    """
+    # Attributes:
+    supervisor = 1  # () 
+    chief_project = 2  # () 
+    respo_lot = 3  # () 
+    respo_metier = 4  # () 
+    simple_user = 5  # () 
+    
+    # Operations
+
