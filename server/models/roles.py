a667145e9eb94bbb0f0ad6fb53411274fa575d08
matthadd <matthis.haddouche@gmail.com>
2021-10-22 22:24:54 +0200

    fix docs

---
+++ server/models/roles.py
@@ -0,0 +1,13 @@
+
+class roles:
+    """Class Roles
+    """
+    # Attributes:
+    supervisor = '1'  # () 
+    chief_project = '2'  # () 
+    respo_lot = '3'  # () 
+    respo_metier = '4'  # () 
+    simple_user = '5'  # () 
+    
+    # Operations
+
